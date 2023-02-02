27eae2929b47de634a259a7be047957e67feb413
Lalit Singh Negi <120033930+Lalit-Negi@users.noreply.github.com>
2023-02-02 18:16:37 +0530

    updated

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(
 	cors({
-		origin: ["http://localhost:5173"],
+		origin: ["http://localhost:5173" , "*"],
 		credentials: true,
 	})
 );
